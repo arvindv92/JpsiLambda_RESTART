c4da3a168b8e2753b3bd2b1035b72bb5aaf884ec
Aravindhan Venkateswaran <arvindvenkat92@gmail.com>
2019-05-03 12:40:25 -0400

    Generator vs reco is switchable with option. Rec wts added to orig tree

--- scripts/ApplyGBWeights.py
+++ scripts/ApplyGBWeights.py
@@ -13,8 +13,13 @@ from sklearn.model_selection import train_test_split
 
 run = int(sys.argv[1])
 mcOpt = int(sys.argv[2])
+isGen = int(sys.argv[3])  # set to 1 to run over generator, 0 to run over rec.
 
 print 'Processing Run', run, 'mcOpt ', mcOpt
+if isGen:
+    print 'Generator\n'
+else:
+    print 'Reconstructed\n'
 columns = ['Lb_P', 'Lb_PT', 'Lb_ETA', 'Jpsi_P', 'Jpsi_PT', 'Jpsi_ETA',
            'L_P', 'L_PT', 'L_ETA', 'p_P', 'p_PT', 'p_ETA', 'pi_P', 'pi_PT',
            'pi_ETA']
@@ -23,46 +28,25 @@ columns = ['Lb_P', 'Lb_PT', 'Lb_ETA', 'Jpsi_P', 'Jpsi_PT', 'Jpsi_ETA',
 mcPath = '../rootFiles/mcFiles/JpsiLambda/JpsiLambda/run{}/'.format(run)
 if mcOpt == 1:
     filePath = '../rootFiles/mcFiles/JpsiLambda/JpsiLambda/run{}/'
-    fileName = 'jpsilambda_pidgen.root'
-    # fileName = 'jpsilambda_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'jpsilambda'
 elif mcOpt == 2:
     filePath = '../rootFiles/mcFiles/JpsiLambda/JpsiSigma/run{}/'
-    fileName = 'jpsisigma_pidgen.root'
-    # fileName = 'jpsisigma_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'jpsisigma'
 elif mcOpt == 3:
     filePath = '../rootFiles/mcFiles/JpsiLambda/JpsiXi/run{}/'
-    fileName = 'jpsixi_pidgen.root'
-    # fileName = 'jpsixi_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'jpsixi'
 elif mcOpt == 6:
     filePath = '../rootFiles/mcFiles/JpsiLambda/Lst1405/run{}/'
-    fileName = 'lst1405_pidgen.root'
-    # fileName = 'jpsixi_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'lst1405'
 elif mcOpt == 7:
     filePath = '../rootFiles/mcFiles/JpsiLambda/Lst1520/run{}/'
-    fileName = 'lst1520_pidgen.root'
-    # fileName = 'jpsixi_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'lst1520'
 elif mcOpt == 8:
     filePath = '../rootFiles/mcFiles/JpsiLambda/Lst1600/run{}/'
-    fileName = 'lst1600_pidgen.root'
-    # fileName = 'jpsixi_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'lst1600'
 elif mcOpt == 9:
     filePath = '../rootFiles/mcFiles/JpsiLambda/chiC1/run{}/'
-    fileName = 'chic1_pidgen.root'
-    # fileName = 'jpsixi_aliased.root'
-    treeName = 'MyTuple'
-    # treeName = 'MCDecayTree'
+    part = 'chic1'
 # elif mcOpt == 4:
 #     filePath = '../rootFiles/mcFiles/JpsiXi/run{}/'
 #     # fileName = 'jpsixi.root'
@@ -70,6 +54,12 @@ elif mcOpt == 9:
 #     # treeName = 'Xib2JpsiXiTree/MyTuple'
 #     treeName = 'MCDecayTree'
 
+if isGen:
+    fileName = part + '.root'
+    treeName = 'MCTuple/MCDecayTree'
+else:
+    fileName = part + '_pidgen.root'
+    treeName = 'MyTuple'
 filePath = filePath.format(run)
 
 # Get the reweighter
@@ -84,10 +74,12 @@ with open(mcPath + 'gb_wts.pkl') as f:
     # Predict weights
     gb_weights = reweighter.predict_weights(original)
     gb_weights.dtype = [('gb_wts', 'float64')]
-    # Write out weights to separate ROOT file
-    # root_numpy.array2root(gb_weights,
-    #                       filePath + 'RW/gbWeights_rec.root',
-    #                       treename='MyTuple', mode='recreate')
-    root_numpy.array2root(gb_weights,
-                          filePath + fileName,
-                          treename='MyTuple', mode='update')
+    if isGen:
+        # Write out weights to separate ROOT file
+        root_numpy.array2root(gb_weights,
+                              filePath + 'RW/gbWeights_rec.root',
+                              treename='MyTuple', mode='recreate')
+    else:
+        root_numpy.array2root(gb_weights,
+                              filePath + fileName,
+                              treename='MyTuple', mode='update')
