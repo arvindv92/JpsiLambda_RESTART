ec39dc703409afdb24aaf6afbdcbf0e43f8ce7a9
Aravindhan Venkateswaran <arvindvenkat92@gmail.com>
2019-05-06 12:56:04 -0400

    trying fc.AdditionalNToysFactor(0.1) to speed up

--- scripts/GetFeldmanCousins.C
+++ scripts/GetFeldmanCousins.C
@@ -50,7 +50,7 @@ void GetFeldmanCousins()
 	// in the model config
 	FeldmanCousins fc(*data,*mc);
 	fc.SetConfidenceLevel(0.95); // 95% interval
-	//fc.AdditionalNToysFactor(0.1); // to speed up the result
+	fc.AdditionalNToysFactor(0.1); // to speed up the result
 	fc.UseAdaptiveSampling(true); // speed it up a bit
 	fc.SetNBins(10); // set how many points per parameter of interest to scan
 	fc.CreateConfBelt(true); // save the information in the belt for plotting
