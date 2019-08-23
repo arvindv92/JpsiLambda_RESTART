91414ddb4d39a392941a169172837942b079e882
Aravindhan Venkateswaran <arvindvenkat92@gmail.com>
2019-08-23 16:34:27 -0400

    Run2 stuff (except gen effs) incorporated

--- scripts/Xib_JpsiLambda.C
+++ scripts/Xib_JpsiLambda.C
@@ -46,11 +46,11 @@ void Xib_JpsiLambda()
 	Float_t Err_BF_Ratio_Run2_wt = 0.0;
 
 	//Fit yield for Xib0 -> J/psi Lambda
-	Float_t N_Xib0_JpsiLambda_Run1 = 6.78;
-	Float_t N_Xib0_JpsiLambda_Run2 = 14.76;
+	Float_t N_Xib0_JpsiLambda_Run1 = 6.1705;//6.78;
+	Float_t N_Xib0_JpsiLambda_Run2 = 17.834;
 
-	Float_t StatErr_N_Xib0_JpsiLambda_Run1 = 2.97;//44% error
-	Float_t StatErr_N_Xib0_JpsiLambda_Run2 = 4.59;//32% error
+	Float_t StatErr_N_Xib0_JpsiLambda_Run1 = 2.73;
+	Float_t StatErr_N_Xib0_JpsiLambda_Run2 = 4.90;
 
 	Float_t SystErr_N_Xib0_JpsiLambda_Run1 = 0.03;
 	Float_t SystErr_N_Xib0_JpsiLambda_Run2 = 0.26;
@@ -169,17 +169,17 @@ void Xib_JpsiLambda()
 	Float_t trackingUnc = 0.05; //5%
 	Float_t xiVtxUnc    = 0.014;//1.4%
 
-	//Get efficiencies
+	//Get efficiencies for Run1
 	TFile *fileIn_nonZero_Run1 = TFile::Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_cutoutks_LL_nonZeroTracks.root","READ");
 	TTree *treeIn_nonZero_Run1 = (TTree*)fileIn_nonZero_Run1->Get("MyTuple");
-	treeIn_nonZero_Run1->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_LL_FinalBDT2_iso2_v0.root");
+	treeIn_nonZero_Run1->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_LL_FinalBDT1_iso2_v1.root");
 
 	TFile *fileIn_Zero_Run1 = TFile::Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_cutoutks_LL_ZeroTracks.root","READ");
 	TTree *treeIn_Zero_Run1 = (TTree*)fileIn_Zero_Run1->Get("MyTuple");
-	treeIn_Zero_Run1->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_zeroTracksLL_FinalBDT2.root");
+	treeIn_Zero_Run1->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run1/xib0_zeroTracksLL_FinalBDT1.root");
 
-	treeIn_nonZero_Run1->Draw("GB_WT>>wt_Run1_nonZero","BDT2 > 0.475","goff");
-	treeIn_Zero_Run1->Draw("GB_WT>>wt_Run1_Zero","BDT2 > 0.365","goff");
+	treeIn_nonZero_Run1->Draw("GB_WT>>wt_Run1_nonZero","BDT1> 0.405","goff");
+	treeIn_Zero_Run1->Draw("GB_WT>>wt_Run1_Zero","BDT1 > 0.335","goff");
 
 	TH1F *wt_Run1_nonZero = (TH1F*)gDirectory->Get("wt_Run1_nonZero");
 	TH1F *wt_Run1_Zero = (TH1F*)gDirectory->Get("wt_Run1_Zero");
@@ -187,7 +187,7 @@ void Xib_JpsiLambda()
 	fstream genFile_Xib0_Run1;
 	genFile_Xib0_Run1.open("../logs/mc/JpsiLambda/Xib0/run1/gen_log.txt");
 
-	genFile_Xib0_Run1>>nGen_Run1; //Get number of generated events
+	genFile_Xib0_Run1>>nGen_Run1; //Get number of generated candidates
 
 	TFile *genWtsFile_Run1 = nullptr;
 	genWtsFile_Run1 = Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run1/RW/gbWeights_gen.root");
@@ -197,7 +197,7 @@ void Xib_JpsiLambda()
 	genWtsTree_Run1->Draw("GB_WT>>genWt_Run1","","goff");
 
 	TH1F *genWt_Run1 = (TH1F*)gDirectory->Get("genWt_Run1");
-	nGen_Run1_wt = genWt_Run1->GetEntries()*genWt_Run1->GetMean();
+	nGen_Run1_wt = genWt_Run1->GetEntries()*genWt_Run1->GetMean(); // Get weighted number of generated candidates
 
 	fstream genEffFile_Run1;
 	genEffFile_Run1.open("../logs/mc/JpsiLambda/Xib0/run1/Generator_Effs_Combined.txt");
@@ -211,8 +211,8 @@ void Xib_JpsiLambda()
 	Float_t num_Run1_wt = (wt_Run1_nonZero->GetMean()*wt_Run1_nonZero->GetEntries()) +
 	                      (wt_Run1_Zero->GetMean()*wt_Run1_Zero->GetEntries());
 
-	Float_t num_Run1 = treeIn_nonZero_Run1->GetEntries("BDT2 > 0.475")
-	                   + treeIn_Zero_Run1->GetEntries("BDT2 > 0.365");
+	Float_t num_Run1 = treeIn_nonZero_Run1->GetEntries("BDT1 > 0.405")
+	                   + treeIn_Zero_Run1->GetEntries("BDT1 > 0.335");
 
 	RecEff_Xib0_JpsiLambda_Run1     = num_Run1*1.0/nGen_Run1;//Calc. reco eff.
 	Err_RecEff_Xib0_JpsiLambda_Run1 = sqrt(RecEff_Xib0_JpsiLambda_Run1*(1-RecEff_Xib0_JpsiLambda_Run1)/nGen_Run1); //statistical error on recon. eff.
@@ -240,6 +240,80 @@ void Xib_JpsiLambda()
 	    <<" % +/- "<<Err_Eff_Xib0_JpsiLambda_Run1_wt*100<<" %"<<endl;
 	cout<<"************************************************"<<endl;
 
+	//Get efficiencies for Run2
+	TFile *fileIn_nonZero_Run2 = TFile::Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run2/xib0_cutoutks_LL_nonZeroTracks.root","READ");
+	TTree *treeIn_nonZero_Run2 = (TTree*)fileIn_nonZero_Run2->Get("MyTuple");
+	treeIn_nonZero_Run2->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run2/xib0_LL_FinalBDT1_iso2_v1.root");
+
+	TFile *fileIn_Zero_Run2 = TFile::Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run2/xib0_cutoutks_LL_ZeroTracks.root","READ");
+	TTree *treeIn_Zero_Run2 = (TTree*)fileIn_Zero_Run2->Get("MyTuple");
+	treeIn_Zero_Run2->AddFriend("MyTuple","../rootFiles/mcFiles/JpsiLambda/Xib0/run2/xib0_zeroTracksLL_FinalBDT1.root");
+
+	treeIn_nonZero_Run2->Draw("GB_WT>>wt_Run2_nonZero","BDT1> 0.475","goff");
+	treeIn_Zero_Run2->Draw("GB_WT>>wt_Run2_Zero","BDT1 > 0.405","goff");
+
+	TH1F *wt_Run2_nonZero = (TH1F*)gDirectory->Get("wt_Run2_nonZero");
+	TH1F *wt_Run2_Zero = (TH1F*)gDirectory->Get("wt_Run2_Zero");
+
+	fstream genFile_Xib0_Run2;
+	genFile_Xib0_Run2.open("../logs/mc/JpsiLambda/Xib0/run2/gen_log.txt");
+
+	genFile_Xib0_Run2>>nGen_Run2; //Get number of generated candidates
+
+	TFile *genWtsFile_Run2 = nullptr;
+	genWtsFile_Run2 = Open("../rootFiles/mcFiles/JpsiLambda/Xib0/run2/RW/gbWeights_gen.root");
+
+	TTree *genWtsTree_Run2 = (TTree*)genWtsFile_Run2->Get("MyTuple");
+
+	genWtsTree_Run2->Draw("GB_WT>>genWt_Run2","","goff");
+
+	TH1F *genWt_Run2 = (TH1F*)gDirectory->Get("genWt_Run2");
+	nGen_Run2_wt = genWt_Run2->GetEntries()*genWt_Run2->GetMean(); // Get weighted number of generated candidates
+
+
+	//@@@@@@@@@@@@@ NO GEN EFFS FOR RUN2 YET. I'M CHEATING AND USING RUN1 GEN EFFS@@@@@@@@@@@@@@@@@
+	fstream genEffFile_Run2;
+	genEffFile_Run2.open("../logs/mc/JpsiLambda/Xib0/run1/Generator_Effs_Combined.txt");
+	//@@@@@@@@@@@@@ NO GEN EFFS FOR RUN2 YET. I'M CHEATING AND USING RUN1 GEN EFFS@@@@@@@@@@@@@@@@@
+	genEffFile_Run2>>GenEff_Xib0_JpsiLambda_Run2;    //Get generator efficiency
+	genEffFile_Run2>>Err_GenEff_Xib0_JpsiLambda_Run2;//and error on above
+
+	cout<<"Run 2 Generator Effs = "<<GenEff_Xib0_JpsiLambda_Run2*100
+	    <<" % +/- "<<Err_GenEff_Xib0_JpsiLambda_Run2*100<<" %"<<endl;
+
+	Float_t num_Run2_wt = (wt_Run2_nonZero->GetMean()*wt_Run2_nonZero->GetEntries()) +
+	                      (wt_Run2_Zero->GetMean()*wt_Run2_Zero->GetEntries());
+
+	Float_t num_Run2 = treeIn_nonZero_Run2->GetEntries("BDT1 > 0.475")
+	                   + treeIn_Zero_Run2->GetEntries("BDT1 > 0.405");
+
+	RecEff_Xib0_JpsiLambda_Run2     = num_Run2*1.0/nGen_Run2;//Calc. reco eff.
+	Err_RecEff_Xib0_JpsiLambda_Run2 = sqrt(RecEff_Xib0_JpsiLambda_Run2*(1-RecEff_Xib0_JpsiLambda_Run2)/nGen_Run2); //statistical error on recon. eff.
+
+	RecEff_Xib0_JpsiLambda_Run2_wt     = num_Run2_wt*1.0/nGen_Run2_wt; //Calc. weighted reco eff.
+	Err_RecEff_Xib0_JpsiLambda_Run2_wt = sqrt(RecEff_Xib0_JpsiLambda_Run2_wt*(1-RecEff_Xib0_JpsiLambda_Run2_wt)/nGen_Run2_wt); //statistical error on weighted recon. eff.
+
+	cout<<"Run 2 UNWEIGHTED Lambda Recons. Effs = "<<RecEff_Xib0_JpsiLambda_Run2*100
+	    <<" % +/- "<<Err_RecEff_Xib0_JpsiLambda_Run2*100<<" %"<<endl;
+
+	cout<<"Run 2 WEIGHTED Lambda Recons. Effs = "<<RecEff_Xib0_JpsiLambda_Run2_wt*100
+	    <<" % +/- "<<Err_RecEff_Xib0_JpsiLambda_Run2_wt*100<<" %"<<endl;
+
+	Eff_Xib0_JpsiLambda_Run2     = RecEff_Xib0_JpsiLambda_Run2*GenEff_Xib0_JpsiLambda_Run2; // Calc. total eff.
+	Err_Eff_Xib0_JpsiLambda_Run2 = Eff_Xib0_JpsiLambda_Run2*sqrt(pow((Err_GenEff_Xib0_JpsiLambda_Run2/GenEff_Xib0_JpsiLambda_Run2),2) +
+	                                                             pow((Err_RecEff_Xib0_JpsiLambda_Run2/RecEff_Xib0_JpsiLambda_Run2),2));                                                                                                                                                                                                                                                                   // and stat error on tot. eff.
+
+	Eff_Xib0_JpsiLambda_Run2_wt     = RecEff_Xib0_JpsiLambda_Run2_wt*GenEff_Xib0_JpsiLambda_Run2; // Calc. total eff.
+	Err_Eff_Xib0_JpsiLambda_Run2_wt = Eff_Xib0_JpsiLambda_Run2_wt*sqrt(pow((Err_GenEff_Xib0_JpsiLambda_Run2/GenEff_Xib0_JpsiLambda_Run2),2) +
+	                                                                   pow((Err_RecEff_Xib0_JpsiLambda_Run2_wt/RecEff_Xib0_JpsiLambda_Run2_wt),2)); // and stat error on tot. eff.
+	cout<<"************************************************"<<endl;
+	cout<<"Run 2 UNWEIGHTED Eff = "<<Eff_Xib0_JpsiLambda_Run2*100
+	    <<" % +/- "<<Err_Eff_Xib0_JpsiLambda_Run2*100<<" %"<<endl;
+	cout<<"Run 2 WEIGHTED Eff = "<<Eff_Xib0_JpsiLambda_Run2_wt*100
+	    <<" % +/- "<<Err_Eff_Xib0_JpsiLambda_Run2_wt*100<<" %"<<endl;
+	cout<<"************************************************"<<endl;
+
+	//calculate BF ratio
 	BF_Ratio_Run1    = (N_Xib0_JpsiLambda_Run1/Eff_Xib0_JpsiLambda_Run1) * B_Xi0_LambdaPi0 / (N_Xib0_JpsiXi0_Run1/Eff_Xibm_JpsiXim_Run1);
 	BF_Ratio_Run1_wt = (N_Xib0_JpsiLambda_Run1/Eff_Xib0_JpsiLambda_Run1_wt) * B_Xi0_LambdaPi0 / (N_Xib0_JpsiXi0_Run1/Eff_Xibm_JpsiXim_Run1_wt);
 
@@ -269,12 +343,12 @@ void Xib_JpsiLambda()
 
 	Err_BF_Ratio_Run1_wt = sqrt(pow(StatErr_BF_Ratio_Run1_wt,2) + pow(SystErr_BF_Ratio_Run1_wt,2));
 
-	//*****Temporarily assume Run2 effs for Xib0->J/psi Lambda is same as Run1 eff, until I get Run 2 MC******
-	Eff_Xib0_JpsiLambda_Run2        = Eff_Xib0_JpsiLambda_Run1;
-	Eff_Xib0_JpsiLambda_Run2_wt     = Eff_Xib0_JpsiLambda_Run1_wt;
-	Err_Eff_Xib0_JpsiLambda_Run2    = Err_Eff_Xib0_JpsiLambda_Run1;
-	Err_Eff_Xib0_JpsiLambda_Run2_wt = Err_Eff_Xib0_JpsiLambda_Run1_wt;
-	//*************************
+	// //*****Temporarily assume Run2 effs for Xib0->J/psi Lambda is same as Run1 eff, until I get Run 2 MC******
+	// Eff_Xib0_JpsiLambda_Run2        = Eff_Xib0_JpsiLambda_Run1;
+	// Eff_Xib0_JpsiLambda_Run2_wt     = Eff_Xib0_JpsiLambda_Run1_wt;
+	// Err_Eff_Xib0_JpsiLambda_Run2    = Err_Eff_Xib0_JpsiLambda_Run1;
+	// Err_Eff_Xib0_JpsiLambda_Run2_wt = Err_Eff_Xib0_JpsiLambda_Run1_wt;
+	// //*************************
 
 	BF_Ratio_Run2    = (N_Xib0_JpsiLambda_Run2/Eff_Xib0_JpsiLambda_Run2) * B_Xi0_LambdaPi0 / (N_Xib0_JpsiXi0_Run2/Eff_Xibm_JpsiXim_Run2);
 	BF_Ratio_Run2_wt = (N_Xib0_JpsiLambda_Run2/Eff_Xib0_JpsiLambda_Run2_wt) * B_Xi0_LambdaPi0 / (N_Xib0_JpsiXi0_Run2/Eff_Xibm_JpsiXim_Run2_wt);
