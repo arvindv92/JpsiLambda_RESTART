8f0889ad60bc191e4da7d789c78c3def36e1cf2d
Aravindhan Venkateswaran <arvindvenkat92@gmail.com>
2019-09-10 12:35:51 -0400

    Updated MCTypes. Also now write out wts for ltimes shifted by 1sigma

--- scripts/ApplyTauWeight.C
+++ scripts/ApplyTauWeight.C
@@ -1,98 +1,81 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TBranch.h"
-#include "iostream"
+#include <iostream>
 
 using namespace std;
 
 void ApplyTauWeight(Int_t run = 1, Int_t mcType = 0, Bool_t isGen = false)
-//mcType = 1 for JpsiLambda MC
-//mcType = 2 for JpsiSigma MC
-//mcType = 6 for JpsiLst(1405) MC
-//mcType = 7 for JpsiLst(1520) MC
-//mcType = 8 for JpsiLst(1600) MC
-//mcType = 9 for chiC1 Lambda MC
-//isGen = true when you are RW'ing Generator Info
+/*
+    mcType = 1 for Lb -> J/psi Lambda MC
+    mcType = 2 for Lb -> J/psi Sigma MC        (reco'd JpsiLambda)
+    mcType = 4 for Lb -> J/psi Lambda*(1405)MC (reco'd JpsiLambda)
+    mcType = 5 for Lb -> J/psi Lambda*(1520)MC (reco'd JpsiLambda)
+    mcType = 6 for Lb -> J/psi Lambda*(1600)MC (reco'd JpsiLambda)
+
+    isGen = true to reweight generator MC, false to reweight reconstructed MC
+ */
 {
 	TFile *fileIn = nullptr, *fileOut = nullptr;
 	TTree *treeIn = nullptr, *treeOut = nullptr;
-	const char *folder = "", *part = "";
-	Double_t Lb_TAU = 0.0;
-	Int_t nEntries = 0;
-	Float_t wt_tau = 0.0;
 
-	cout<<"********Processing Run "<<run<<" MC Type "<<mcType<<" isGen "<<isGen<<endl;
+	const char *folder = "", *part = "";
+
+	Double_t Lb_TAU = 0.0;
+
+	Int_t nEntries = 0;
+
+	Float_t wt_tau = 0.0, wt_tau_plus = 0.0, wt_tau_minus = 0.0;
+
+	cout<<"******** Processing Run "<<run<<" MC Type "<<mcType<<" isGen "<<isGen<<endl;
+
 	switch(mcType)
 	{
 	case 0:
 	{
 		folder = "";
-		part = "";
+		part   = "";
 		break;
 	}
 	case 1:
 	{
 		folder = "JpsiLambda";
-		part = "jpsilambda";
+		part   = "jpsilambda";
 		break;
 	}
 	case 2:
 	{
 		folder = "JpsiSigma";
-		part = "jpsisigma";
-		break;
-	}
-	case 3:
-	{
-		folder = "JpsiXi";
-		part = "jpsixi";
+		part   = "jpsisigma";
 		break;
 	}
 	case 4:
 	{
-		folder = "Bu_JpsiX";
-		part = "bu_jpsix";
+		folder = "Lst1405";
+		part   = "lst1405";
 		break;
 	}
 	case 5:
 	{
-		folder = "Bd_JpsiX";
-		part = "bd_jpsix";
+		folder = "Lst1520";
+		part   = "lst1520";
 		break;
 	}
 	case 6:
 	{
-		folder = "Lst1405";
-		part = "lst1405";
-		break;
-	}
-	case 7:
-	{
-		folder = "Lst1520";
-		part = "lst1520";
-		break;
-	}
-	case 8:
-	{
 		folder = "Lst1600";
-		part = "lst1600";
+		part   = "lst1600";
 		break;
 	}
-	case 9:
+	default:
 	{
-		folder = "chiC1";
-		part = "chic1";
-		break;
-	}
-	case 10:
-	{
-		folder = "JpsiKs";
-		part = "jpsiks";
-		break;
+		cout<<"$$$ MC Type doesn't match any of the allowed cases. Exiting! $$$"<<endl;
+		exit(1);
 	}
 	}
+
 	if(!isGen)
-		fileIn = TFile::Open(Form("../rootFiles/mcFiles/JpsiLambda/%s/run%d/%s_triggered.root",folder,run,part),"UPDATE");
+		fileIn = TFile::Open(Form("../rootFiles/mcFiles/JpsiLambda/%s/run%d/%s_pidgen.root",folder,run,part),"UPDATE");
 	else
 		fileIn = TFile::Open(Form("../rootFiles/mcFiles/JpsiLambda/%s/run%d/%s.root",folder,run,part),"READ");
 
@@ -113,7 +96,9 @@ void ApplyTauWeight(Int_t run = 1, Int_t mcType = 0, Bool_t isGen = false)
 		treeOut = new TTree("MyTuple","MyTuple");
 	}
 
-	TBranch *wtBranch = treeOut->Branch("wt_tau",&wt_tau,"wt_tau/F");
+	TBranch *wtBranch       = treeOut->Branch("wt_tau",&wt_tau,"wt_tau/F");
+	TBranch *wtBranch_plus  = treeOut->Branch("wt_tau_plus",&wt_tau_plus,"wt_tau_plus/F");
+	TBranch *wtBranch_minus = treeOut->Branch("wt_tau_minus",&wt_tau_minus,"wt_tau_minus/F");
 
 	nEntries = treeIn->GetEntries();
 
@@ -135,21 +120,29 @@ void ApplyTauWeight(Int_t run = 1, Int_t mcType = 0, Bool_t isGen = false)
 		}
 
 		treeIn->GetEntry(i);
+		//World average for Lb lifetime is (1.470 +/- 0.010) ps  (0.7% uncertainty).
 		if(run == 1)
 		{
-			wt_tau = exp(-1000*Lb_TAU/1.470)/exp(-1000*Lb_TAU/1.425);
+			wt_tau       = exp(-1000*Lb_TAU/1.470)/exp(-1000*Lb_TAU/1.425);
+			wt_tau_plus  = exp(-1000*Lb_TAU/1.480)/exp(-1000*Lb_TAU/1.425);
+			wt_tau_minus = exp(-1000*Lb_TAU/1.460)/exp(-1000*Lb_TAU/1.425);
 		}
 		else if(run == 2)
 		{
-			wt_tau = exp(-1000*Lb_TAU/1.470)/exp(-1000*Lb_TAU/1.450);
+			wt_tau       = exp(-1000*Lb_TAU/1.470)/exp(-1000*Lb_TAU/1.450);
+			wt_tau_plus  = exp(-1000*Lb_TAU/1.480)/exp(-1000*Lb_TAU/1.450);
+			wt_tau_minus = exp(-1000*Lb_TAU/1.460)/exp(-1000*Lb_TAU/1.450);
 		}
 		if(Lb_TAU > 1)
 		{
 			wt_tau = 1.0;
 		}
-		// treeOut->Fill();
 		if(!isGen)
+		{
 			wtBranch->Fill();
+			wtBranch_plus->Fill();
+			wtBranch_minus->Fill();
+		}
 		else
 			treeOut->Fill();
 	}
